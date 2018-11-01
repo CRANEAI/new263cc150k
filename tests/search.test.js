fffe72b7deb2a3e54b534ed162cdf435fb91ba1c
Elly <elly@mg.crane.ai>
2018-10-31 21:57:13 -0400

    updated tests/search refactoring

--- tests/search.test.js
+++ tests/search.test.js
@@ -7,10 +7,10 @@
  * new
 */
 
-/* view has actions which trigger on events, view should should error messages and display activity indicator when processing */
-it("view has actions which trigger on events, view should should error messages and display activity indicator when processing", () => {
+/* search view has a Label located at the bottom center of the view with text space */
+it("search view has a Label located at the bottom center of the view with text space", () => {
   /* ToDo developer needs to complete unit test 
-               view has actions which trigger on events, view should should error messages and display activity indicator when processing
+               search view has a Label located at the bottom center of the view with text space
                uncomment below and update the code to test
                 //instance.action(function(error) {
                 //  if (error) throw error;
