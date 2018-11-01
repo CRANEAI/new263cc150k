a8be8d933b496d33d395e28e2d14c7b003af9a8a
Elly <elly@mg.crane.ai>
2018-10-31 23:27:58 -0400

    updated client/src/reducers/index refactoring

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -1,16 +1,16 @@
 import { combineReducers } from "redux"
 import { reducer as Form } from "redux-form"
 
-import edit_profile from "./reducer_edit_profile"
 import search from "./reducer_search"
 import chat from "./reducer_chat"
 import sign_up from "./reducer_sign_up"
+import edit_profile from "./reducer_edit_profile"
 
 const rootReducer = combineReducers({
-  edit_profile: edit_profile,
   search: search,
   chat: chat,
-  sign_up: sign_up
+  sign_up: sign_up,
+  edit_profile: edit_profile
 })
 
 export default rootReducer
