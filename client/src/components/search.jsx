0b0bea0e1ec173e31ea0a64954ae8a3d6d3ba31a
Elly <elly@mg.crane.ai>
2018-10-31 21:57:20 -0400

    updated client/src/components/search refactoring

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -7,18 +7,12 @@ created by: FusionCode
 // Component Requirements
 //----------------------------------------------------------------------------------------------------------
 
-the sign up view has a status bar located at the very-top across the full width of the view
-the sign up view has a Headline Label located at the top left of the view, correct text needs defined
-sign up view has Label located at the top left of the view, text needs identified
-sign up view has Label located at the top left of the view, text needs identified
-sign up view has Label located at the top left of the view, text needs identified
-sign up view has Label located at the mid-top left of the view, text needs identified
-sign up view has Label located at the mid-top left of the view, text needs identified
-sign up view has Label located at the main area left of the view, text needs identified
-the sign up view has a Pager located at the main area left of the view, correct text needs defined
-sign up view has Label located at the bottom center of the view, text needs identified
-sign up view has Label located at the bottom center of the view, text needs identified
-view has actions which trigger on events, view should should error messages and display activity indicator when processing
+the search view has a status bar located at the very-top across the full width of the view
+search view has Navigation Bar located at the very-top in the center of the view with text search
+search view has Label located at the very-top center of the view, text needs identified
+the search view has a Input Box located at the very-top in the center of the view, correct text needs defined
+the search will appear on focus with state is set
+search view has a Label located at the bottom center of the view with text space
 
 */
         
